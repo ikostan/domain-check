8cea15786c86664093ce53d8c71b9071aaf5d3ed
notroot <notroot@debian8.vcn.ca>
2016-11-09 21:00:26 -0800

    new version

--- Domain-Check_v4.0/domain-check_v4.0.py
+++ Domain-Check_v4.0/domain-check_v4.0.py
@@ -4,13 +4,18 @@ import sys
 import os
 import logging
 import random
-import csv      # Allows handle CSV files
-import codecs   # UTF-8 encoded BOM. in order to get rid of \xef\xbb\xbf in a list read from a file
-import socket   # Low-level networking interface
-import numpy    # Data manipulation
-import shutil   # High-level file operations
-import tld      # Extracts the top level domain (TLD) from the URL given (pip install tld).
-import time     # This module provides various time-related functions
+import csv                                      # Allows handle CSV files
+import codecs                                   # UTF-8 encoded BOM. in order to get rid of \xef\xbb\xbf in a list read from a file
+import socket                                   # Low-level networking interface
+import numpy                                    # Data manipulation
+import shutil                                   # High-level file operations
+import tld                                      # Extracts the top level domain (TLD) from the URL given (pip install tld).
+import time                                     # This module provides various time-related functions
+import smtplib                                  # Import smtplib for the actual sending function
+from email.mime.text import MIMEText            # Import the email modules we'll need
+from email.mime.multipart import MIMEMultipart  # Allows to send a MIME message
+#from mailer import Mailer
+#from mailer import Message
 
 
 # System variables:
@@ -24,6 +29,7 @@ import time     # This module provides various time-related functions
 # file_path = os.path.join(os.getcwd(), ipList_file)        # Path to ip_list.csv
 # file_path = os.path.join(os.getcwd(), template_file)      # Path to template.csv
 # file_path = os.path.join(os.getcwd(), ipList_file)        # Path to vcn_ip.range.csv
+# file_path = os.path.join(os.getcwd(), Config_file)        # Path to config.txt
 
 
 def get_file_path(fileName):
@@ -38,7 +44,7 @@ template_file = get_file_path('template.csv')     #Empty template file
 LOG_FILENAME = get_file_path('logging.log')       #Log file
 VCN_IP_RANGE = get_file_path('vcn_ip_range.csv')  #List with vcn ips
 WHOIS_file = get_file_path('WHOIS.csv')           #List with WHOIS results
-
+Config_file = get_file_path('config.txt')         #Config file: SMTP server + port
 
 # Write log messages to a log file and the console at the same time:
 if os.path.exists(LOG_FILENAME):
@@ -92,12 +98,15 @@ logging.debug('A total number of IPs to validate: ', num)
 from tld import get_tld
 
 def get_domain_name(url):
+    logging.info('Start get_domain_name function')
     domain_name = get_tld(url) #Get Top Level Domain Name
     return domain_name
+    logging.info('Finish get_domain_name function')
 
 
 #Clean email string:
 def clean_email(email):
+    logging.info('Start clean_email function')
     new = ""                                            #A new string
     if "Registrant Email:" in email:
         old = "['Registrant Email:"                     #String to replase - condition 1.1
@@ -118,14 +127,18 @@ def clean_email(email):
                 old = " "                               #String to replase
                 email = str.replace(email, old, new)    #Replace method
     elif "['e-mail:" in email:
-        old = "['e-mail:"                               #string to replase - condition 3.1
+        old = "['e-mail:"                               #String to replase - condition 3.1
         email = str.replace(email, old, new)            #replace method
-        if "']" in email:                               #string to replase - condition 3.2
+        if "']" in email:                               #string to replase - condition 3.2.1
             old = "']"                                  #string to replase
             email = str.replace(email, old, new)        #replace method
-            if " " in email:                            #Strip all whitespace from string
-                old = " "                               #string to replase
-                email = str.replace(email, old, new)    #Replace method   
+            if " AT " in email:                         #string to replase - condition 3.2.2
+                old = " AT "                            #string to replase
+                new_1 = "@"
+                email = str.replace(email, old, new_1)  #Replace method
+                if " " in email:                        #to strip all whitespace from string
+                    old = " "
+                    email = str.replace(email, old, new)#replace method   
     elif "['Admin Email:" in email:
         old = "['Admin Email:"                          #string to replase - condition 4.1
         email = str.replace(email, old, new)            #replace method
@@ -155,10 +168,12 @@ def clean_email(email):
                 email = str.replace(email, old, new)    #replace method    
       
     return email                                        #return the result
+    logging.info('Finish clean_email function')
 
 
 # Read from csv file function:
 def read_csv(domainList_file):
+    logging.info('Start domainList_file function')
     domains = {} #a smart list of domain names - it has an index starts from 1
     i = 0
     with codecs.open(domainList_file, 'rU', 'utf-8-sig') as csvFile: #Open file
@@ -178,6 +193,7 @@ def read_csv(domainList_file):
     return domains
     return ip_list
     csvFile.Close()
+    logging.info('Finish domainList_file function')
 
 domains = read_csv(domainList_file) #call read_csv function in order to create smart domains list
 n = len(domains)                    #A total number of domain names in the list
@@ -188,6 +204,7 @@ logging.debug('A total number of domain names in the test list: ', n)
 
 # WHOIS function:
 def get_whois(url):
+    logging.info('Start get_whois function')
     command = "whois " + url                                        #run whois command from CLI
     process = os.popen(command)                                     #open a new CLI as a process
     whois_result = str(process.read())                              #get WHOIS result from the CLI and save is as a string "whois_result"
@@ -195,10 +212,12 @@ def get_whois(url):
         for row in whois_result:
             file_handler.write(row)              
     return whois_result
+    logging.info('Finish get_whois function')
 
 
 #Read WHOIS_file file in order to create smart whois_rows list:
 def read_whois(WHOIS_file):
+    logging.info('Start read_whois function')
     i = 0
     whois_rows = {}
     with codecs.open(WHOIS_file,'rU','utf-8-sig') as csvWHOIS1: #Open file
@@ -208,10 +227,12 @@ def read_whois(WHOIS_file):
                     i += 1
     return whois_rows
     csvWHOIS1.Close()
+    logging.info('Finish read_whois function')
 
 
 #Get Email from WHOIS_file function:
 def get_email(WHOIS_file):
+    logging.info('Start get_email function')
     emails = {}
     with codecs.open(WHOIS_file,'rU','utf-8-sig') as csvWHOIS: #Open file
         read_whois = csv.reader(csvWHOIS) #Read file
@@ -229,10 +250,84 @@ def get_email(WHOIS_file):
             i += 1
     return emails
     csvWHOIS.Close()
+    logging.info('Finish get_email function')
+
+
+# Send an Email function:
+def send_Email(recipient):
+    logging.info('Start send_Email function')
+
+    # Send the message via local SMTP server.
+    # Read config file:
+    logging.info('Read config file')
+
+    read_config = open(Config_file, 'r') #Open file
+
+    SMTP_server = read_config.readline()
+    SMTP_server = SMTP_server.strip("\xef\xbb\xbf")
+    SMTP_server = SMTP_server.strip("\r\n")
+
+    port = read_config.readline()
+    port = port.strip("\r\n")
+
+    sender = read_config.readline()
+    sender = sender.strip("\r\n")
+
+    mail = smtplib.SMTP(SMTP_server, port)
+    mail.ehlo()
+    mail.starttls()
+
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "Link"
+    msg['From'] = sender
+    msg['To'] = recipient
+
+    # Create the body of the message (a plain-text and an HTML version).
+    text = "Hello.\nThis is sample text." #a plain-text
+    #an HTML version
+    html = """\ 
+    <html>
+    <head></head>
+    <body>
+        <p>
+            Hello.<br>
+            This is sample text.
+        </p>
+    </body>
+    </html>
+    """
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html, 'html')
+
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+
+    logging.info('Ask for username')
+    username = raw_input("Please enter your username: ") #ask for username
+    logging.info('Ask for password')
+
+    import getpass
+    password = getpass.getpass()        #ask for password
+    #print 'You entered:', password     #for debuging only 
+
+    logging.info('Login in to email account')
+    mail.login(username, password)
+
+    logging.info('Send Email')
+    mail.sendmail(sender, recipient, msg.as_string())
+    mail.quit()
+    logging.info('Finish send_Email function')
 
 
 # Write to csv file function:
 def write_csv(ipList_file):
+    logging.info('Start write_csv function')
     with codecs.open(ipList_file,'a','utf-8-sig') as csvFile2: #Open file
         writer = csv.writer(csvFile2) #Write to file
         i = 1                         #start index from 1
@@ -248,6 +343,7 @@ def write_csv(ipList_file):
                         logging.debug('Found valid IP: ' + domains[i] + ': '+ row[0] + " matches " + domainIP)
                         isValid = 1
             csvFile3.close() #close the file
+
             if isValid == 0:
                 # Removing www.
                 # This is a bad idea, because www.python.org could 
@@ -271,7 +367,7 @@ def write_csv(ipList_file):
 
                 site_whois = get_whois(domain_url) #call get_whois function in order to create WHOIS.csv file with whois data
                 
-                time.sleep(1) # delays for 5 seconds
+                time.sleep(1) # Wait for x second
                 
                 emails = get_email(WHOIS_file)     #call get_email function in order to extract email from whois data
                 emails_num = len(emails)           #get total number of emails
@@ -296,24 +392,36 @@ def write_csv(ipList_file):
                 elif emails_num == 4:
                      emails[4] = 'null'
 
-                emails[0] = clean_email(emails[0]) #call clean_email unction in order to clean up all the garbage from the string
-                emails[1] = clean_email(emails[1]) #call clean_email unction in order to clean up all the garbage from the string
-                emails[2] = clean_email(emails[2]) #call clean_email unction in order to clean up all the garbage from the string
-                emails[3] = clean_email(emails[3]) #call clean_email unction in order to clean up all the garbage from the string
-                emails[4] = clean_email(emails[4]) #call clean_email unction in order to clean up all the garbage from the string
+                emails[0] = clean_email(emails[0]) #call clean_email function in order to clean up all the garbage from the string
+                emails[1] = clean_email(emails[1]) #call clean_email function in order to clean up all the garbage from the string
+                emails[2] = clean_email(emails[2]) #call clean_email function in order to clean up all the garbage from the string
+                emails[3] = clean_email(emails[3]) #call clean_email function in order to clean up all the garbage from the string
+                emails[4] = clean_email(emails[4]) #call clean_email function in order to clean up all the garbage from the string
                 
-                writer.writerow([domains[i], domainIP, 'invalid', emails[0], emails[1], emails[2], emails[3], emails[4]])     #Invalid IP - write domain name, ip address and INVALID comment
-                logging.info('Writing: domains[i], domainIP, invalid, emails[0], emails[1], emails[2], emails[3], emails[4]')
-                logging.debug(domains[i], domainIP, 'invalid', emails[0], emails[1], emails[2], emails[3], emails[4])
+                #Invalid IP - write domain name, ip address and INVALID comment
+                writer.writerow([domains[i], domainIP, 'invalid', emails[0], emails[1], emails[2], emails[3], emails[4], 'true', 'not implemented', 'not implemented'])
+
+                send_Email(emails[1]) #call send email function
+                
+                #logs
+                logging.info('Writing: domains[i], domainIP, invalid, emails[0], emails[1], emails[2], emails[3], emails[4], true, not implemented, not implemented')
+                logging.debug(domains[i], domainIP, 'invalid', emails[0], emails[1], emails[2], emails[3], emails[4], 'true', 'not implemented', 'not implemented')
                 os.remove(WHOIS_file) #remove WHOIS_file
             else:
-                writer.writerow([domains[i], domainIP, 'valid', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a'])     #write domain name and ip address, ip address and VALID comment
+                #write domain name and ip address, ip address and VALID comment
+                writer.writerow([domains[i], domainIP, 'valid', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'false', 'n/a', 'not implemented'])  
+                #logs
+                logging.info('Writing:[domains[i], domainIP, valid, n/a, n/a, n/a, n/a, n/a, false, n/a, not implemented')
+                logging.debug(domains[i], domainIP, 'valid', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'false', 'n/a', 'n/a')
+
             i += 1      #increase index by 1
     csvFile2.close()    #close the file
+    logging.info('Finish write_csv function')
     
 domains = write_csv(ipList_file) #call write_csv function
 
 os.remove(VCN_IP_RANGE) #remove VCN_IP_RANGE
 print 'FINISHED'
 logging.info('FINISHED')
+
 #THE END
